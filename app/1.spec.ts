b2b7f1860daecf20ade9dc78e8a36981db041f13
iliion <1ll1as10ann0u@gmail.com>
2016-11-07 17:58:31 +0200

    Initial setup for Unit Testing

---
+++ app/1.spec.ts
@@ -0,0 +1,3 @@
+describe('Test that unit testing is working!!', () => {
+  it('true is true', () => expect(true).toBe(true));
+});
--- app/app.component.ts
+++ app/app.component.ts
@@ -4,4 +4,5 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: '<h1>My First Angular App</h1>'
 })
+
 export class AppComponent { }
