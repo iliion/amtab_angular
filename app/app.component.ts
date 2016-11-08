9ec3f21b24eef2c4cf0ed4255040a19e8394868c
iliion <1ll1as10ann0u@gmail.com>
2016-11-08 15:52:13 +0200

    Add List of Buses and display it. Upon clicking diplay moe details about a bus

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
 
+export class Bus {
+  id: number;
+  name: string;
+}
+
+const BUSES: Bus[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
+
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular App</h1>'
+  templateUrl: 'content.html'
 })
 
-export class AppComponent { }
+export class AppComponent {
+  title = '';
+  buses = BUSES;
+  selectedBus: Bus;
+  onSelect(bus:Bus):void {
+  	this.selectedBus = bus;
+  }
+}
